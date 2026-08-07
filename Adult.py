7c7e6f0134445ddda8ab51c15ac600923a1d97d0
maneesh20100205-spec <maneesh20100205@gmail.com>
2026-08-07 17:42:46 -0400

    Branch e (#18)
    * conflict_2
    
    * conflict_3
    
    * minor or major python code which includes strings, int, booleans.
    
    ---------
    
    Co-authored-by: maneesh_bhai <maneeshgitt@gmail.com>

---
+++ Adult.py
@@ -0,0 +1,10 @@
+name = "Maneesh"
+age = 16 
+adult = age >= 18
+
+print(name)
+print(age)
+print(adult)
+
+if adult: print("You are an adult.")
+else: print("You are a minor.")
--- test.js
+++ test.js
@@ -3,3 +3,4 @@ const banana = 10;
 const orange = 15;
 const total = apple + banana + orange;
 const average = total / 3;
+console.log(`Total: ${total}`); 
